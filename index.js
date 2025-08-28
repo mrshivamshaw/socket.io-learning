e69db07703ab6db00e1a34673200aaaa867f77b1
mrshivamshaw <shivamshaw9005@gmail.com>
2025-08-29 00:21:22 +0530

    initialize project with basic server and HTML setup with handshake

---
+++ index.js
@@ -0,0 +1,16 @@
+const app = require("express")();
+const http = require("http").Server(app);
+const  io = require("socket.io")(http);
+const path = require("path")
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"index.html"))
+})
+
+io.on("connection", ()=>{
+    console.log("user connected");
+})
+
+http.listen(3000, ()=>{
+    console.log("Server is running on port 3000");
+})
